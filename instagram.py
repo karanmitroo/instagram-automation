b59cd4d11c4069382d6a8d8e67fd2e3e0d8dd638
Karan Mitroo <karan1.mitroo@gmail.com>
2017-07-04 14:13:11 +0530

    Atom Beautify

--- instagram.py
+++ instagram.py
@@ -18,6 +18,7 @@ def login_from_fb(b, username, password):
     login_button = b.find_element_by_id('loginbutton')
     login_button.click()
 
+
 def login_from_insta(b, username, password):
     login_username = b.find_elements_by_xpath('//input')[0]
     login_password = b.find_elements_by_xpath('//input')[1]
@@ -25,7 +26,10 @@ def login_from_insta(b, username, password):
     login_password.send_keys(password)
     log_in = b.find_element_by_xpath('//button[contains(text(), "Log in")]')
     log_in.click()
-login_via_fb = raw_input("If you want to login via Facebook press y/Y else login via instagram credentials by pressing n/N: ")
+
+
+login_via_fb = raw_input(
+    "If you want to login via Facebook press y/Y else login via instagram credentials by pressing n/N: ")
 
 if login_via_fb.lower() == 'y':
     username = raw_input("Enter your Facebook username: ")
@@ -37,7 +41,8 @@ else:
     password_match = getpass.getpass("Enter your Instagram password again: ")
 
 while (password != password_match):
-    password = getpass.getpass("Your password did NOT match. Please enter your password again: ")
+    password = getpass.getpass(
+        "Your password did NOT match. Please enter your password again: ")
     password_match = getpass.getpass("Enter your password again: ")
 
 
@@ -45,11 +50,14 @@ while (password != password_match):
 CREATING FOLDER WITH THE NAME OF THE
 PERSON YOU WANT TO DOWNLOAD PICTURES OF
 '''
-friend = raw_input("Enter the username of the person you want to like and download all the photos of: ")
+friend = raw_input(
+    "Enter the username of the person you want to like and download all the photos of: ")
 if os.path.exists(friend):
-    folder_name = raw_input("The folder with the name '" + friend + "' already exists. Enter the name of folder you want to save all photos to: ")
+    folder_name = raw_input("The folder with the name '" + friend +
+                            "' already exists. Enter the name of folder you want to save all photos to: ")
     while os.path.exists(folder_name):
-        folder_name = raw_input("The folder with the name '" + folder_name + "' also exists. Enter the name of folder you want to save all photos to: ")
+        folder_name = raw_input("The folder with the name '" + folder_name +
+                                "' also exists. Enter the name of folder you want to save all photos to: ")
 
 else:
     folder_name = friend
@@ -64,19 +72,21 @@ b.get('http://instagram.com')
 FINDING THE LOGIN BUTTON TO GO TO THE LOGIN PAGE
 '''
 if login_via_fb.lower() == 'y':
-        try:
-            log_in = WebDriverWait(b, 20).until(
-                EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "Facebook")]'))
-                )
-        except:
-            b.quit()
-        log_in.click()
+    try:
+        log_in = WebDriverWait(b, 20).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '//button[contains(text(), "Facebook")]'))
+        )
+    except:
+        b.quit()
+    log_in.click()
 
 else:
     try:
         log_in = WebDriverWait(b, 20).until(
-            EC.presence_of_element_located((By.XPATH, '//a[contains(text(), "Log in")]'))
-            )
+            EC.presence_of_element_located(
+                (By.XPATH, '//a[contains(text(), "Log in")]'))
+        )
     except:
         b.quit()
     log_in.click()
@@ -104,8 +114,9 @@ LOADING MORE PICTURES IN THEIR PROFILE
 '''
 try:
     load_more = WebDriverWait(b, 10).until(
-        EC.presence_of_element_located((By.XPATH, '//a[contains(text(), "Load more")]'))
-        )
+        EC.presence_of_element_located(
+            (By.XPATH, '//a[contains(text(), "Load more")]'))
+    )
     load_more.click()
 except:
     pass
@@ -125,37 +136,44 @@ b.execute_script("window.scrollTo(0, 0);")
 all IS THE VARIABLE NAME THAT IS THE DIV THAT CONTAINS ALL THE PICTURES
 '''
 all = b.find_element_by_xpath('//article/div/div')
+
+
 def do(b, all):
     pictures = all.find_elements_by_xpath('./div/div')
     for pic in pictures:
         pic.click()
         time.sleep(1)
 
-        #TRY IF YOU GET A VIDEO OR ELSE DOWNLOAD THE IMAGE.
+        # TRY IF YOU GET A VIDEO OR ELSE DOWNLOAD THE IMAGE.
         try:
             src = WebDriverWait(b, 3).until(
-                EC.presence_of_element_located((By.XPATH, '//article/div/div/div/div/div/div/div//video'))
-                )
-            src =  src.get_attribute('src')
-            urllib.urlretrieve(src, os.getcwd() + '/' + folder_name + '/' + src.split('/')[-1])
+                EC.presence_of_element_located(
+                    (By.XPATH, '//article/div/div/div/div/div/div/div//video'))
+            )
+            src = src.get_attribute('src')
+            urllib.urlretrieve(src, os.getcwd() + '/' +
+                               folder_name + '/' + src.split('/')[-1])
         except:
-            #FOUND TWO XPATHS FOR IMAGES. INSTAGRAM RANDOMLY PLACES THEM IN ONE OF THEM.
+            # FOUND TWO XPATHS FOR IMAGES. INSTAGRAM RANDOMLY PLACES THEM IN ONE OF THEM.
             try:
                 src = WebDriverWait(b, 3).until(
-                    EC.presence_of_element_located((By.XPATH, '//article/div/div/div/div/img'))
-                    )
+                    EC.presence_of_element_located(
+                        (By.XPATH, '//article/div/div/div/div/img'))
+                )
             except:
                 src = WebDriverWait(b, 3).until(
-                    EC.presence_of_element_located((By.XPATH, '//article/div/div/div/div/div/img'))
-                    )
-            src =  src.get_attribute('src')
-            urllib.urlretrieve(src, os.getcwd() + '/' + folder_name + '/' + src.split('/')[-1])
+                    EC.presence_of_element_located(
+                        (By.XPATH, '//article/div/div/div/div/div/img'))
+                )
+            src = src.get_attribute('src')
+            urllib.urlretrieve(src, os.getcwd() + '/' +
+                               folder_name + '/' + src.split('/')[-1])
         finally:
             pass
 
-
         try:
-            liked = b.find_element_by_xpath('//article/div[2]/section[1]/a[1]/span[contains(text(), "Like")]')
+            liked = b.find_element_by_xpath(
+                '//article/div[2]/section[1]/a[1]/span[contains(text(), "Like")]')
             liked.click()
         except:
             pass
@@ -166,5 +184,7 @@ def do(b, all):
         # text.send_keys(str(random()) + Keys.RETURN)
         cross = b.find_element_by_xpath('//body/div[2]/div/button')
         cross.click()
-do(b,all)
+
+
+do(b, all)
 b.quit()
